97ae2ff7daa05f4a18f8bc926d9562231b03bb1f
Filippo Lanza <fpg.lanza@gmail.com>
2020-08-17 20:30:34 +0200

    Added working longest-word game, no score

---
+++ app/controllers/games_controller.rb
@@ -0,0 +1,25 @@
+require 'open-uri'
+
+class GamesController < ApplicationController
+  def new
+    @letters = Array.new(10) { ("A".."Z").to_a.sample }
+  end
+
+  def score
+    @letters = params[:letters].split
+    @word = (params[:word] || "").upcase
+    @key = params[:authenticity_token]
+    @included = included?(@word, @letters)
+    @english_word = english_word?(@word)
+  end
+
+  def included?(word, letters)
+    word.chars.all? { |letter| word.count(letter) <= letters.count(letter) }
+  end
+
+  def english_word?(word)
+    response = open("https://wagon-dictionary.herokuapp.com/#{word}")
+    json = JSON.parse(response.read)
+    json['found']
+  end
+end
